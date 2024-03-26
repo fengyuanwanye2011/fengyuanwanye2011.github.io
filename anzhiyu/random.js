8d4e58de14ca29be98776aedf5956476c7857290
“fengyuanwanye2011” <chenjianan110607@outlook.com>
2024-03-26 19:20:51 +0800

    Site updated: 2024-03-26 19:20:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/25/hello-world/","2024/03/25/关于新站开放/"];function toRandomPost(){
+var posts=["2024/03/25/关于新站开放/","2024/03/26/奇幻服务器官网/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
